658dfa8f91b977bef7e45c4f348d4c56b1fe3e55
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 18:38:45 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -4,26 +4,4 @@ const divide = (x = 2000, y = 100) => x / y;
     
 const square = (x = 2) => x * x;
 
-console.log(divide());
-/*
-  it("has an arrow function called square", () => {
-    expect(square).to.exist
-  })
-
-  it("square arrow function takes one parameter and multiplies it times itself", () => {
-    let x = 2
-    expect(square(x)).to.equal(4)
-  })
-
-  it("has an arrow function called add", () => {
-    expect(add).to.exist
-  })
-
-  it("add arrow function takes two parameters and adds them together", () => {
-    let a = 3
-    let b = 4
-    expect(add(a,b)).to.equal(a+b)
-  })
-})
-
-*/
+const add = (a = 3, b = 4) => a + b;
